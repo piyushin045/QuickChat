2f1927054262c70f2df1f82e7bbc76f7bd73a3a9
piyush <piyusi045@gmail.com>
2025-09-28 12:22:41 +0530

    Enhance ChatContainer and RightSidebar with improved layout and functionality; update LoginPage for better user experience and form handling.

--- client/src/components/ChatContainer.jsx
+++ client/src/components/ChatContainer.jsx
@@ -12,9 +12,9 @@ const ChatContainer = ({selectedUser, setSelectedUser}) => {
     },[])
   
   return selectedUser ? (
-    <div className='h-full overflow-scrol relative backdrop-blur-lg'>
+    <div className='h-full overflow-scroll relative backdrop-blur-lg'>
       {/*---- header area */}
-      <div className='flex item-center gap-3 py-3 mx-4 border-b border-stone-500'>
+      <div className='flex items-center gap-3 py-3 mx-4 border-b border-stone-500'>
         <img src={assets.profile_martin} alt='' className='w-8 rounded-full' />
         <p className='flex-1 text-lg text-white flex items-center gap-2'>
           Martin Johnson
@@ -28,7 +28,7 @@ const ChatContainer = ({selectedUser, setSelectedUser}) => {
       <div className='flex flex-col h-[calc(100%-120px)] overflow-y-scroll p-3
       pb-6'>
         {messagesDummyData.map((msg, index)=>(
-          <div key={index} className={`flex item-end gap-2 justify-end ${msg.senderId !== '680f50e4f10f3cd28382ecf9' && 'flex-row-reverse'}`}>
+          <div key={index} className={`flex items-end gap-2 justify-end ${msg.senderId !== '680f50e4f10f3cd28382ecf9' && 'flex-row-reverse'}`}>
             {msg.image ? (
               <img className='max-w-[230px] border border-gray-700 rounded-lg overflow-hidden mb-8 ' src={assets.img1} alt='' />
             ):(
@@ -42,13 +42,27 @@ const ChatContainer = ({selectedUser, setSelectedUser}) => {
 
           </div>
         ))}
+        <div ref={scrollEnd}></div>
       </div>
 
-      <div ref={scrollEnd}></div>
-
+      {/*------------ bottom area */}
+      <div className='absolute bottom-0 left-0 right-0 flex items-center gap-3 p-3'>
+        <div className='flex-1 flex items-center bg-gray-100/12 px-3 rounded-full'>
+          <input type='text' placeholder='Send a message'
+            className='flex-1 text-sm p-3 border-none rounded-lg outline-none
+            text-white placeholder-gray-400'
+          />
+          <input type='file' id='image' accept='image/png, image/jpeg' hidden />
+          <label htmlFor='image'>
+            <img src={assets.gallery_icon} alt='' className='w-5 mr-2 cursor-pointer'/>
+          </label>
+        </div>
+         <img src={assets.send_button} alt='' className='w-7 cursor-pointer' />
+      </div>
+     
     </div>
   ) : (
-    <div className='flex flex-col item-center justify-center gap-2 text-gray-500 
+    <div className='flex flex-col items-center justify-center gap-2 text-gray-500 
     bg-white/10 max-md:hidden'>
       <img src={assets.logo_icon} alt='' className='max-w-16' />
       <p className='text-lg font-medium text-white'>Chat anytime, anywhere</p>
--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
+import assets from '../assets/assets'
 
 const LoginPage = () => {
+
+  const [currState, setCurrState] = useState("Sign Up")
+
   return (
-    <div>
-        <h1>LoginPage</h1>
+    <div className='min-h-screen bg-cover bg-center flex items-center
+    justify-center gap-8 sm:justify-evenly max-sm:flex-col backdrop-blur-2xl'>
+      {/*---------- left----------- */}
+      <img src={assets.logo_big} alt='' className='w-[min(30vw,250px)]' />
+      {/*-----------right-------- */}
+
+      <form className='border-2 bg-white/8 text-white border-gray-500 p-6 flex
+      flex-col gap-6 rounded-lg shadow-lg'>
+          <h2>{currState}</h2>
+          <input type='text' placeholder='Full Name' />
+          <input type='text' placeholder='Email Address' />
+          <input type='text' placeholder='Password' />
+
+          <button className='bg-gradient-to-r from-purple-400 to-violet-600 text-white border-none 
+      text-sm font-light rounded-full cursor-pointer'>
+        Create Account
+      </button>
+      <input type='checkbox' placeholder='i have read all the document carefully' />
+      </form>
     </div>
   )
 }
