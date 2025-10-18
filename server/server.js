90d74efece69e1390e58541f4aba19f8b37906a7
piyushin045 <piyushin045@gmail.com>
2025-10-18 13:38:21 +0530

    fix: update background image path and add server export for versal

--- server/server.js
+++ server/server.js
@@ -59,7 +59,13 @@ await connectDB();
 
 
 
-
-
+if(process.env.NODE_ENV !== "production"){
+    
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, ()=> console.log("server is running on PORT:" +PORT))
+
+}
+
+// export server for versal
+
+export default server;
